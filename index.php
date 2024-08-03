d0331cb75991198b750072c23cf48795cd827e6c
JITENDRA KUMAR <99076224+Jitendraunatti@users.noreply.github.com>
2024-08-03 11:15:21 +0530

    minor changes

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (file_exists('Channel.json')) {
         $print_data .= '#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha' . PHP_EOL;
         $print_data .= '#KODIPROP:inputstream.adaptive.license_key=' . $Host_Url . 'widevine.php?id=' . $for_js->id . PHP_EOL;
         $print_data .= '#EXTVLCOPT:http-user-agent=plaYtv/7.1.5 (Linux;Android 13) ExoPlayerLib/2.11.7' . PHP_EOL;
-        $print_data .= '#EXTINF:-1 tvg-id="' . $for_js->id . '" tvg-logo="' . $for_js->logo . '" group-title="Extra-Tv-Team ' . $for_js->genre . '",' . $for_js->title . PHP_EOL;
+        $print_data .= '#EXTINF:-1 tvg-id="' . $for_js->id . '" tvg-logo="' . $for_js->logo . '" group-title="JITENDRAUNATTI' . $for_js->genre . '",' . $for_js->title . PHP_EOL;
         $print_data .= $Host_Url . 'mpd.php?id=' . $for_js->id . PHP_EOL;
     }
 
